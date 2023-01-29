ef4b0508258bde88a511cf8168829838920b1115
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2023-01-29 23:23:24 +0600

    completed the task

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const stars = document.querySelectorAll(".stars i");
+const showStars = document.getElementById("show-rating");
 // console.log(stars);
 stars.forEach((star, index) =>{
   star.addEventListener('click', () =>{
     // console.log("clicked", index + 1);
     stars.forEach((star, index2) =>{
       index >= index2 ? star.classList.add("active") : star.classList.remove("active");
+      showStars.innerHTML = index + 1;
     })
   })
 } )
