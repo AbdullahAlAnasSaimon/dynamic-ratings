e6ef1b7e7e6a113edd518666723d6f1844d6b347
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2023-01-29 23:19:16 +0600

    added style file and active class

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const stars = document.querySelectorAll(".stars i");
 // console.log(stars);
 stars.forEach((star, index) =>{
   star.addEventListener('click', () =>{
-    console.log("clicked", index + 1);
+    // console.log("clicked", index + 1);
     stars.forEach((star, index2) =>{
-      console.log(index2 + 1);
+      index >= index2 ? star.classList.add("active") : star.classList.remove("active");
     })
   })
 } )
